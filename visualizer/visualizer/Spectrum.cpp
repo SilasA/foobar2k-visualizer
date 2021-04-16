7702dd5b7457d77de586c9e03726c10b519daf58
Silas Agnew <agnewsilas@gmail.com>
2021-04-16 02:17:16 -0400

    Added rolling average

--- visualizer/visualizer/Spectrum.cpp
+++ visualizer/visualizer/Spectrum.cpp
@@ -6,22 +6,71 @@
 #include <cmath>
 #include <algorithm>
 
+#define DATA_SIZE 576
+
 #define SPECTRUM_BEGIN 3
 #define SPECTRUM_END 515
 #define SPECTRUM_RANGE (SPECTRUM_END - SPECTRUM_BEGIN)
 
-Spectrum::Spectrum(winampVisModule* mod, RECT dimensions, int bars)
+float Spectrum::UpdateCurrentAverage() 
+{
+	// Convert traditional arrays and add new data
+	std::array<float, DATA_SIZE> temp[2];
+
+	// Left
+	for (int i = 0; i < DATA_SIZE; i++) {
+		temp[0][i] = (float)m_mod->spectrumData[0][i];
+		temp[1][i] = (float)m_mod->spectrumData[1][i];
+	}
+	m_spectrumRolling[0].push_back(temp[0]);
+	m_spectrumRolling[1].push_back(temp[1]);
+
+	// Pop oldest (once full)
+	// both maintain the same size
+	if (m_spectrumRolling[0].size() > m_rollingMaxSize) {
+		m_spectrumRolling[0].pop_front();
+		m_spectrumRolling[1].pop_front();
+	}
+
+	// Clear averaged data
+	for (int i = 0; i < DATA_SIZE; i++) {
+		m_spectrumRollingAvg[0][i] = 0;
+		m_spectrumRollingAvg[1][i] = 0;
+	}
+
+	// Sum history data
+	for (spec_iter it0 = m_spectrumRolling[0].begin(), it1 = m_spectrumRolling[1].begin();
+		it0 != m_spectrumRolling[0].end() && it1 != m_spectrumRolling[1].end();
+		it0++, it1++)
+	{
+		for (int i = 0; i < DATA_SIZE; i++) {
+			m_spectrumRollingAvg[0][i] += (*it0)[i];
+			m_spectrumRollingAvg[1][i] += (*it1)[i];
+		}
+	}
+
+	// Make average
+	for (int i = 0; i < DATA_SIZE; i++) {
+		m_spectrumRollingAvg[0][i] /= (float)m_spectrumRolling[0].size();
+		m_spectrumRollingAvg[1][i] /= (float)m_spectrumRolling[1].size();
+	}
+}
+
+Spectrum::Spectrum(winampVisModule* mod, RECT dimensions, int bars, int rollingAvgSize)
 {
 	m_mod = mod;
 
 	m_dimensions = dimensions;
 	m_bars = bars;
 
-	m_spectrumBars = new float[m_bars];
+	m_rollingMaxSize = rollingAvgSize;
+	
+	// Visual and compiled bar data
+	m_spectrumBars = new float[bars];
 
 	// Proportion of peak data and spectrum dimensions
 	int height = m_dimensions.top - m_dimensions.bottom;
-	m_barScale = height / std::log2((float)SPECTRUM_RANGE / (m_bars / 2.0f) * 255);
+	m_barScale = height / std::log10((float)SPECTRUM_RANGE / (m_bars / 2.0f) * 255);
 }
 
 Spectrum::~Spectrum()
@@ -42,7 +91,10 @@ int Spectrum::Init()
 
 int Spectrum::Render()
 {
-	// Clear Spectrum
+	// Update current averages
+	UpdateCurrentAverage();
+
+	// Clear bar data
 	for (int i = 0; i < m_bars; i++) {
 		m_spectrumBars[i] = 0;
 	}
@@ -50,11 +102,10 @@ int Spectrum::Render()
 	// Goal:
 	// Map 0->576 x2 => 3->515 x2
 
-
 	// Left (reverse fill for mirror effect)
 	int div = SPECTRUM_RANGE / (m_bars / 2);
 	for (int i = 0; i < SPECTRUM_RANGE; i++) {
-		m_spectrumBars[(SPECTRUM_RANGE - i - 1) / div] += m_mod->spectrumData[0][i + SPECTRUM_BEGIN];
+		m_spectrumBars[(SPECTRUM_RANGE - i - 1) / div] += m_spectrumRollingAvg[0][i + SPECTRUM_BEGIN];
 #ifdef DEBUG
 		//m_log << "col " << i << ": " << (int)m_mod->spectrumData[0][i] << std::endl;
 #endif
@@ -62,7 +113,7 @@ int Spectrum::Render()
 
 	// Right
 	for (int i = 0; i < SPECTRUM_RANGE; i++) {
-		m_spectrumBars[(m_bars + i) / div] += m_mod->spectrumData[1][i + SPECTRUM_BEGIN];
+		m_spectrumBars[(m_bars + i) / div] += m_spectrumRollingAvg[1][i + SPECTRUM_BEGIN];
 	}
 
 	// TODO: Trim and scale
--- visualizer/visualizer/Spectrum.h
+++ visualizer/visualizer/Spectrum.h
@@ -3,27 +3,39 @@
 
 #include "vis_visualizer.h"
 
+#include <deque>
+#include <array>
+
 #ifdef DEBUG
 #include <fstream>
 #endif
 
+typedef std::deque<std::array<float, 576>>::iterator spec_iter;
+
 class Spectrum
 {
 private:
+	std::deque<std::array<float, 576>> m_spectrumRolling[2];
+
+	float	m_spectrumRollingAvg[2][576];
 	float*	m_spectrumBars;
 	int		m_bars;
+	int		m_rollingMaxSize;
 	float	m_barScale;
 
 	RECT m_dimensions;
 
 	winampVisModule* m_mod;
 
+	// Rolling average functions
+	float UpdateCurrentAverage();
+
 #ifdef DEBUG
 	std::ofstream m_log;
 #endif
 
 public:
-	Spectrum(winampVisModule* mod, RECT dimensions, int bars);
+	Spectrum(winampVisModule* mod, RECT dimensions, int bars, int rollingAvgSize = 5);
 	~Spectrum();
 
 	int Init();
